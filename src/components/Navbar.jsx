d92d330915f870dbd72623a475e740b2756d3b92
amandena <manda.m.johnson@gmail.com>
2022-09-06 22:45:22 -0400

    finished set up of navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -9,6 +10,7 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `
 
 const Left = styled.div`
@@ -21,17 +23,37 @@ const Language = styled.span`
   cursor: pointer;
 `
 const SearchContainer = styled.div`
-  border: 1px solid lightgray;
+  border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input = styled.input`
+  border: none;
 `
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`
+
+const Logo = styled.h1`
+  font-weight: bold;
 `
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `
 
 const Navbar = () => {
@@ -41,12 +63,21 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            input
-            <Search />
+            <Input />
+            <Search style={{color: "gray", fontSize: "16px"}} />
           </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center><Logo>ringsnthingsbyamanda</Logo></Center>
+        <Right>
+
+          <MenuItem>SIGN UP</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   )
