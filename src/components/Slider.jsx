134da395ee1a2a0d71d28575ef5dcb0e673429c2
amandena <manda.m.johnson@gmail.com>
2022-09-09 14:52:59 -0400

    added more elements to slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -22,6 +22,52 @@ const Arrow = styled.div`
   left: ${props => props.direction === "left" && "10px"};
   right: ${props => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+`
+
+const Slide = styled.div` 
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`
+
+const ImgContainer = styled.div` 
+  height: 100%;
+  flex: 1;
+`
+
+const Image = styled.img`
+  height: 80%;
+`
+
+const InfoContainer = styled.div` 
+  flex: 1;
+  padding: 50px;
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+`
+
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `
 
 const Slider = () => {
@@ -30,6 +76,18 @@ const Slider = () => {
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://variety.com/wp-content/uploads/2022/01/Lady-Gaga-Variety-Actors-on-Actors-3-16x9-1.jpg" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Description>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Description>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
