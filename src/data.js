e61357552615e602dab5d4f1ec4b4ce445dc6529
amandena <manda.m.johnson@gmail.com>
2022-09-12 21:33:32 -0400

    added slider data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://vitalcy-images.s3.amazonaws.com/website/home/ladyguyjump.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://vitalcy-images.s3.amazonaws.com/website/home/ladyguyjump.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://vitalcy-images.s3.amazonaws.com/website/home/ladyguyjump.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+]
