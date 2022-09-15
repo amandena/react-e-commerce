f7f8d612fbd70e72d09411784c37e9a097d1ac52
amandena <manda.m.johnson@gmail.com>
2022-09-14 22:28:46 -0400

    fixed keys inside mapped items

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -13,7 +13,7 @@ const Categories = () => {
   return (
     <Container>
       {categories.map(item => (
-        <CategoryItem key={item} item={item} />
+        <CategoryItem key={item.id} item={item} />
       ))}
     </Container>
   )
