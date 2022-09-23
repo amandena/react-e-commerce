839382f0277527137c48cce89260b1e86d7e5d3e
amandena <manda.m.johnson@gmail.com>
2022-09-23 14:47:21 -0400

    imported cart

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <div className="App">
-      <Login />
+      <Cart />
     </div>
   );
 }
