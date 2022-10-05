90154be0a5751ab9a6cb8075a3dcf7767ab2ea7a
amandena <manda.m.johnson@gmail.com>
2022-10-05 19:35:18 -0400

    connected to mongo db and configured dotenv

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
-const port = 5001
+dotenv.config()
 
-app.listen(port, () => {
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => {
+    console.log(err)
+  })
+
+
+app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server is running!")
 })
