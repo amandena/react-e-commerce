a31c2e0cbca700004298d2dc60d0945223720ead
amandena <manda.m.johnson@gmail.com>
2022-10-07 18:45:53 -0400

    fixed order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const CartSchema = new mongoose.Schema(
+const OrderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
     products: [
@@ -9,8 +9,11 @@ const CartSchema = new mongoose.Schema(
         quantity: { type: Number, default: 1 }
       }
     ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "Pending" }
   }, 
   { timestamps: true }
 )
 
-module.exports = mongoose.model("Cart", CartSchema)
+module.exports = mongoose.model("Order", OrderSchema)
