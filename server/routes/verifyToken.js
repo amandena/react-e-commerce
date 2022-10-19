6e4e5a9ebf079a96d6aa5ee4de36cdeac8762105
amandena <manda.m.johnson@gmail.com>
2022-10-18 22:58:50 -0400

    created a function to verify token and if a user is admin

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -24,4 +24,14 @@ const verifyTokenAndAuth = (req, res, next) => {
   });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuth }
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin }
