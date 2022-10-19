6c158104695ee4f6f4390ebf0e122527247739ec
amandena <manda.m.johnson@gmail.com>
2022-10-19 15:30:06 -0400

    created delete and find user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const User = require("../models/User")
-const { verifyToken, verifyTokenAndAuth } = require("./verifyToken")
+const { verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin } = require("./verifyToken")
 
 // UPDATE
 router.put("/:id", verifyTokenAndAuth, async (req, res) => {
@@ -13,6 +13,27 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
       $set: req.body 
     }, {new: true})
     res.status(200).json(updatedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted!")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const {password, ...others} = user._doc
+    res.status(200).json(others)
   } catch (err) {
     res.status(500).json(err)
   }
