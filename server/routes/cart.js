326a42b50c9f2f0e09bd86eaeea1c6cad2deeb7d
amandena <manda.m.johnson@gmail.com>
2022-11-11 18:50:28 -0500

    created update cart route

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -15,16 +15,17 @@ router.post("/", verifyToken, async (req, res) => {
 })
 
 // UPDATE
-// router.put("/:id", verifyTokenAndAuth, async (req, res) => {
-//   try {
-//     const updatedProduct = await Product.findOneAndUpdate(req.params.id, {
-//       $set: req.body 
-//     }, {new: true})
-//     res.status(200).json(updatedProduct)
-//   } catch (err) {
-//     res.status(500).json(err)
-//   }
-// })
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findOneAndUpdate(
+      req.params.id, {
+      $set: req.body 
+    }, {new: true})
+    res.status(200).json(updatedCart)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 // //DELETE
 // router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
